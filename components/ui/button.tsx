cd4cc16cd42b3df7cd51b34e67a6a20fd436a649
ss251 <ss251@uw.edu>
2024-09-27 16:49:55 +0530

    fix: remove hover styles for difficulty button in mobile

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -9,7 +9,7 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default: "bg-neutral-900 text-neutral-50 hover:bg-neutral-900/90 dark:bg-neutral-50 dark:text-neutral-900 dark:hover:bg-neutral-50/90",
+        default: "bg-neutral-900 text-neutral-50 dark:bg-neutral-50 dark:text-neutral-900",
         destructive:
           "bg-red-500 text-neutral-50 hover:bg-red-500/90 dark:bg-red-900 dark:text-neutral-50 dark:hover:bg-red-900/90",
         outline:
@@ -18,7 +18,7 @@ const buttonVariants = cva(
           "bg-neutral-100 text-neutral-900 hover:bg-neutral-100/80 dark:bg-neutral-800 dark:text-neutral-50 dark:hover:bg-neutral-800/80",
         ghost: "hover:bg-neutral-100 hover:text-neutral-900 dark:hover:bg-neutral-800 dark:hover:text-neutral-50",
         link: "text-neutral-900 underline-offset-4 hover:underline dark:text-neutral-50",
-        touch: "bg-neutral-900 text-neutral-50 active:bg-neutral-900/90 dark:bg-neutral-50 dark:text-neutral-900 dark:active:bg-neutral-50/90",
+        touch: "border border-neutral-200 bg-white dark:border-neutral-800 dark:bg-neutral-950 active:bg-neutral-100 active:text-neutral-900 dark:active:bg-neutral-800 dark:active:text-neutral-50",
       },
       size: {
         default: "h-10 px-4 py-2",
